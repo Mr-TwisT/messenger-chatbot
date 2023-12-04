3f1b7e4916e9f7ab24dbaf090ba0f2adc99fc972
Patryk Szymik <patryk.szymik01@wp.pl>
2023-12-04 15:26:53 +0100

    create postback file

--- handlers/postback.js
+++ handlers/postback.js
@@ -1,1 +1,13 @@
-const handlePostback = (sender_psid, received_postback) => {};
+const handlePostback = (sender_psid, received_postback) => {
+  let response;
+
+  let payload = received_postback.payload;
+
+  if (payload === 'yes') {
+    response = { text: 'Thanks!' };
+  } else if (payload === 'no') {
+    response = { text: 'Oops, try sending another image.' };
+  }
+
+  callSendAPI(sender_psid, response);
+};
