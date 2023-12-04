6d0b4fa353420ee7d7ba402b7fe604a3e633cb9c
Patryk Szymik <patryk.szymik01@wp.pl>
2023-12-04 15:10:18 +0100

    add image handler

--- handlers/message.js
+++ handlers/message.js
@@ -3,11 +3,40 @@ const request = require('request');
 
 const handleMessage = (sender_psid, received_message) => {
   let response;
-  console.log(received_message);
 
   if (received_message.text) {
     response = {
       text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  } else if (received_message.attachments) {
+    let attachment_url = received_message.attachments[0].payload.url;
+
+    response = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title: 'Is this the right picture?',
+              subtitle: 'Tap a button to answer.',
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Yes!',
+                  payload: 'yes',
+                },
+                {
+                  type: 'postback',
+                  title: 'No!',
+                  payload: 'no',
+                },
+              ],
+            },
+          ],
+        },
+      },
     };
   }
 
